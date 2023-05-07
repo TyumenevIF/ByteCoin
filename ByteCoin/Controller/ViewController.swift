650cab0c099b067d6ec48997eb18e034b774ffb4
Ilyas Tyumenev <tumenoff@mail.ru>
2023-05-07 18:28:15 +0300

    Initial Commit

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Ilyas Tyumenev on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Ilyas Tyumenev on 07.05.2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
